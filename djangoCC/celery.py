4e163e00a287c242aa8b604c7f5543f3097d17e4
Harshavii <harshavibodkhe@gmail.com>
2023-11-14 12:51:36 +0530

    updating

--- djangoCC/celery.py
+++ djangoCC/celery.py
@@ -1,15 +1,15 @@
-from __future__ import absolute_import, unicode_literals
-import os
-from celery import Celery
+# from __future__ import absolute_import, unicode_literals
+# import os
+# from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoCC.settings')
+# # set the default Django settings module for the 'celery' program.
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoCC.settings')
 
-# create a Celery instance and configure it using the settings from Django
-celery_app = Celery('djangoCC')
+# # create a Celery instance and configure it using the settings from Django
+# celery_app = Celery('djangoCC')
 
-# Load task modules from all registered Django app configs.
-celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+# # Load task modules from all registered Django app configs.
+# celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Auto-discover tasks in all installed apps
-celery_app.autodiscover_tasks()
+# # Auto-discover tasks in all installed apps
+# celery_app.autodiscover_tasks()
